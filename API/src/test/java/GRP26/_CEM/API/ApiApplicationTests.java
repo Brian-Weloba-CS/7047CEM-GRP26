e588c34d67bdd69a99e5c0051adef764b4707931
Brian-Weloba <bweloba@gmail.com>
2024-10-23 14:35:10 +0100

    Project setup and scaffolding

---
+++ API/src/test/java/GRP26/_CEM/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package GRP26._CEM.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
